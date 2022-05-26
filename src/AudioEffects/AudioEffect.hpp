4cdfc75102c5c4e52a9d52972b90f1035bef1299
JugadK <curaww@gmail.com>
2022-05-26 16:23:41 -0700

    [add] getName getter to AudioEffect

--- src/AudioEffects/AudioEffect.hpp
+++ src/AudioEffects/AudioEffect.hpp
@@ -1,9 +1,10 @@
 #pragma once
-
 #include <juce_core/juce_core.h>
 
 class AudioEffect {
     public:
-        virtual void processAudio(float &sample) = 0;
-        
+
+        // processAudio applies the AudioEffect effect onto the sample
+        virtual void processAudio(float &sample) = 0;        
+        virtual juce::String getName() = 0;
 };
